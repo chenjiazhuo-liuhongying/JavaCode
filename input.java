d92f26a82d57e1f369113b11309c4e7c418ccb94
练佳子 <10229005+lian-jiazi@user.noreply.gitee.com>
2022-05-09 09:51:16 +0000

    update input.java.

--- input.java
+++ input.java
@@ -5,8 +5,10 @@ import java.util.Scanner;
 public class input {
     //输入类
     public static void main(String[] args) {
+        //创建对象
         Scanner sc = new Scanner(System.in);
         System.out.println("请输入");
+        // 接受输入值
         int x = sc.nextInt();
         System.out.println("x:" + x);
 
